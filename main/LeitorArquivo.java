58b60a5e16057ceb7ce77660f07ed33030a7b2ff
Bleitonfreitas <bleitonfreitass@gmail.com>
2025-06-02 21:47:35 -0300

    Delete LeitorArquivo.java
    Erro no salvamento

--- main/LeitorArquivo.java
+++
@@ -1,8 +0,0 @@
-import org.apache.commons.io.FileUtils;
-import opennlp.tools.tokenize.SimpleTokenizer;
-import edu.stanford.nlp.pipeline.*;
-
-public class LeitorArquivo {
-
-    public static void main(String[] args) throws IOException {
-        File file = new File("texto.txt");
