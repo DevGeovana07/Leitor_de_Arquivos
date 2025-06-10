0930175068b4e1e2ad2c0a800927b82f893ed2a3
Bleiton <bleitonfreitass@gmail.com>
2025-06-10 19:05:55 -0300

    erros invisíveis
    corrigindo erros

--- main/LeitorArquivo.java
+++ main/LeitorArquivo.java
@@ -4,9 +4,9 @@ import edu.stanford.nlp.pipeline.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
-
- public class leitorArquivo {
+public class LeitorArquivo {
 
     public static void main(String[] args) throws IOException {
         File file = new File("texto.txt");
@@ -14,7 +14,6 @@ import java.io.IOException;
         // 1. Leitura do arquivo
         String conteudo = FileUtils.readFileToString(file, "UTF-8");
         System.out.println("Conteúdo do arquivo:\n" + conteudo);
-
 
         // 2. Contar palavras e linhas
         int linhas = conteudo.split("\r\n|\r|\n").length;
@@ -26,15 +25,15 @@ import java.io.IOException;
         Properties props = new Properties();
         props.setProperty("annotators", "tokenize,ssplit,pos,lemma,parse,sentiment");
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        
+
         CoreDocument doc = new CoreDocument(conteudo);
         pipeline.annotate(doc);
 
-System.out.println("\nAnálise de Sentimentos:");
+        System.out.println("\nAnálise de Sentimentos:");
         for (CoreSentence sentence : doc.sentences()) {
             String sent = sentence.text();
             String sentiment = sentence.sentiment();
             System.out.println("[" + sentiment + "] " + sent);
-        }
-    }
+        }
+    }
 }
